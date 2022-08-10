a8ccebf1aea0cb0969753cfe9079af1455cd4028
fexikus <felix.sobanski@gmail.com>
2022-08-10 05:54:21 -0700

    Completed Async function

--- src/components/functions/async.jsx
+++ src/components/functions/async.jsx
@@ -1,3 +1,12 @@
 const myAsyncFunction = async () => {
+    const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await usersResponse.json();
+    const SecondUser = users[1];
 
+    console.log(SecondUser);
+
+    const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts?userId=' + SecondUser.id);
+
+    const posts = await postsResponse.json();
+    console.log(posts);
 }
