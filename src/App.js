2e6a8ed71289dc151cd4046a672484cb489a72eb
Felix <felix.sobanski@gmail.com>
2022-08-10 04:16:32 -0700

    Added async Function and Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {Routes,Route} from 'react-router-dom';
+
+import SignIn from './components/routes/signin/signin.component';
 import Home from './components/routes/home/home.component';
-import Shop from './components/routes/shop/shop.component.jsx';
+import Shop from './components/routes/shop/shop.component';
 import Navigation from './components/routes/navigation/navigation.component';
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index={true} element={<Home />}/>
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
