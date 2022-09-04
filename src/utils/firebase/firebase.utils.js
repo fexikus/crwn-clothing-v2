06b2ed7058c780904a2c5a537a3c8322ba5ea168
fexikus <felix.sobanski@gmail.com>
2022-09-04 21:22:37 +0200

    Firebase Auth angefangen

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,6 +2,8 @@
 
 import { initializeApp } from "firebase/app";
 import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+
+import { getFirestore,doc,getDoc,setDoc } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -35,6 +37,16 @@ provider.setCustomParameters({
     prompt: "select_account"
 });
 
-export const auth = getAuth();
 
+
+
+export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+
+
+export const db = getFirestore();
+
+const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid );
+  
+}
